b0898545582c3814670e9a2e28a0a2b5eff4f5f6
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-07-31 21:28:32 +0200

    feat: implement fetch products machine

--- build-config/webpack.development.js
+++ build-config/webpack.development.js
@@ -1,1 +1,5 @@
-module.exports = () => ({});
+const devServerConfig = require('../dev-server/webpack.server.config');
+
+module.exports = () => ({
+  devServer: devServerConfig,
+});
--- src/parts/app/app.component.jsx
+++ src/parts/app/app.component.jsx
@@ -1,12 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useMachine } from '@xstate/react';
+
+import { productsMachine } from '../../machine/products.machine';
 
 import './app.styles.css';
 
-export const App = () => (
-  <div className="container">
-    <h3>Lista produktów</h3>
-    <ul>
-      <li className="row">Patelnia, cena: 89,99zł</li>
-    </ul>
-  </div>
-);
+export function App() {
+  const [current, send] = useMachine(productsMachine);
+
+  useEffect(() => {
+    send('FETCH');
+  }, []);
+
+  function sendRetry() {
+    send('RETRY');
+  }
+
+  function sendRefresh() {
+    send('REFRESH');
+  }
+
+  const renderProductsList = () => {
+    if (current.matches('loading')) {
+      return <div>Ładowanie listy produktów...</div>;
+    }
+
+    if (current.matches('failure')) {
+      return (
+        <div>
+          Nie udało się pobrac listy produktów...
+          <button type="button" onClick={sendRetry}>
+            Spróbuj pobrać listę produktów ponownie
+          </button>
+        </div>
+      );
+    }
+
+    if (current.matches('loaded')) {
+      return (
+        <ul>
+          {React.Children.toArray(
+            current.context.products.map((_product) => (
+              <li className="row">{`${_product.name}, cena: ${_product.price}zł`}</li>
+            )),
+          )}
+        </ul>
+      );
+    }
+
+    return null;
+  };
+
+  function renderRefreshButton() {
+    if (current.matches('loaded')) {
+      const formattedDate = new Date(current.context.lastUpdated).toISOString();
+
+      return (
+        <button type="button" onClick={sendRefresh}>
+          {`Odśwież listę produktów (ostatnio odświeżono ${formattedDate})`}
+        </button>
+      );
+    }
+
+    return null;
+  }
+
+  return (
+    <div className="container">
+      {renderRefreshButton()}
+      <h3>Lista produktów</h3>
+      {renderProductsList()}
+    </div>
+  );
+}
